f67324e3e33e54a2fb3902817f9d7c38ea82217c
tksharpless <tksharpless@336479b0-e29e-8c40-a810-faafe343d7c6>
2008-10-12 03:01:28 +0000

    Add GPL notice to all source files

--- src/GLwindow.h
+++ src/GLwindow.h
@@ -1,4 +1,20 @@
 /* GLwindow.h for pvQt_1
+ * Copyright (C) 2008 Thomas K Sharpless
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this file; if not, write to Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
 
   A "glue widget", required to make pvQtView work inside QMainWindow.
   Also provides command line and input file handling.
--- src/pictureTypes.h
+++ src/pictureTypes.h
@@ -1,4 +1,23 @@
-/* pictureTypes.h
+/* pictureTypes.h for pvQt	10 Oct 2008
+ * Copyright (C) 2008 Thomas K Sharpless
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this file; if not, write to Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+
+  The picture type names visible to the user, and their attributes.
+
 */
 #ifndef PICTURETYPES_H
 #define PICTURETYPES_H
