3c382836cb3609fc986ac58896c69282bc300db1
tksharpless <8539366+tksharpless@users.noreply.github.com>
2008-10-12 03:01:28 +0000

    Add GPL notice to all source files

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,20 @@
-/* main.cpp for freepvQt 08Sep2008
+/* main.cpp for pvQt 08Sep2008
+ * Copyright (C) 2008 Thomas K Sharpless
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this file; if not, write to Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
 */
 
 #include <QApplication>
--- src/pvQtView.h
+++ src/pvQtView.h
@@ -1,4 +1,25 @@
-/* pvQtView.h 
+/* pvQtView.h for pvQt 11 Sept 2008
+ * Copyright (C) 2008 Thomas K Sharpless
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this file; if not, write to Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+
+ pvQtView is an OpenGL display widget that shows images projected on
+ a 3D spherical or cylindrical screen.
+
+ This class is the only one in pvQt that issues OpenGL calls.
 
 */
 #ifndef PVQTVIEW_H
